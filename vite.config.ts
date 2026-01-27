59c76fb6b24ccf50437cfc82131ec76ba856b9a0
lightzane <bhest.pat@gmail.com>
2026-01-27 11:06:48 +0800

    fix: blank page using correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/seal/', // <-- github pages base path (repo name)
   plugins: [tailwindcss()],
   resolve: {
     alias: {
